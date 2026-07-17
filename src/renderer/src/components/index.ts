959b081b0b63b618728c067eab64812a513f0ba2
satish9177 <satishkommanaboina@gmail.com>
2026-07-18 01:49:54 +0530

    feat(renderer): add explanation and confirmation cards

---
+++ src/renderer/src/components/index.ts
@@ -0,0 +1,4 @@
+export { ExplanationCard } from './ExplanationCard'
+export type { ExplanationCardProps } from './ExplanationCard'
+export { ToolConfirmationCard } from './ToolConfirmationCard'
+export type { ToolConfirmationCardProps } from './ToolConfirmationCard'
